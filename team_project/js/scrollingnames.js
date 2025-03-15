578f856a487cdc5857581591af59a585d6c4d061
Nora Pence <nora.pence@bellevuecollege.edu>
2025-03-15 14:26:29 -0700

    Update scrollingnames.js

--- team_project/js/scrollingnames.js
+++ team_project/js/scrollingnames.js
@@ -1,1 +1,10 @@
+const container = document.querySelector('.scrolling-text');
+let amount = 0;
 
+setInterval(() => {
+ amount += 10;
+ container.scrollLeft = amount;
+ if (amount >= container.scrollWidth) {
+  amount = 0;
+ }
+}, 20);
